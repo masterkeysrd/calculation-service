4822ab310d1e9f1460dd5a1e89b0136f4d601d2b
David Morales <morales.david1997@gmail.com>
2023-05-19 20:35:44 -0400

    feat(domain/user): :sparkles: add fake user repository

--- internal/pkg/domain/user/repository.go
+++ internal/pkg/domain/user/repository.go
@@ -1,9 +1,86 @@
 package user
 
+import (
+	"errors"
+	"time"
+)
+
 type Repository interface {
-	FindByID(id uint64) (User, error)
-	FindByUserName(userName string) (User, error)
-	Create(user User) error
-	Update(user User) error
-	Delete(user User) error
+	FindByID(id uint64) (*User, error)
+	FindByUserName(userName string) (*User, error)
+	Create(user *User) error
+	Update(user *User) error
+	Delete(user *User) error
+}
+
+type FakeUserRepository struct {
+	counter uint64
+	users   []*User
+}
+
+func NewFakeUserRepository() Repository {
+	return &FakeUserRepository{
+		counter: 1,
+		users: []*User{
+			{
+				ID:        1,
+				UserName:  "admin@test.com",
+				Password:  "admin",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+		},
+	}
+}
+
+func (r *FakeUserRepository) FindByID(id uint64) (*User, error) {
+	for _, u := range r.users {
+		if u.ID == id {
+			return u, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (r *FakeUserRepository) FindByUserName(userName string) (*User, error) {
+	for _, u := range r.users {
+		if u.UserName == userName {
+			return u, nil
+		}
+	}
+
+	return nil, nil
+}
+
+func (r *FakeUserRepository) Create(user *User) error {
+	r.counter++
+	user.ID = r.counter
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	r.users = append(r.users, user)
+
+	return nil
+}
+
+func (r *FakeUserRepository) Update(user *User) error {
+	for i, u := range r.users {
+		if u.ID == user.ID {
+			r.users[i].UpdatedAt = time.Now()
+			r.users[i] = user
+		}
+	}
+
+	return nil
+}
+
+func (r *FakeUserRepository) Delete(user *User) error {
+	for i, u := range r.users {
+		if u.ID == user.ID {
+			r.users = append(r.users[:i], r.users[i+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("user not found")
 }
