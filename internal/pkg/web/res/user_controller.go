a40c40cf2be2424c1af2ec1102261ac62f114b05
David Morales <morales.david1997@gmail.com>
2023-05-19 20:44:35 -0400

    feat(web/res): :sparkles: add basic user controller

---
+++ internal/pkg/web/res/user_controller.go
@@ -0,0 +1,55 @@
+package res
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/masterkeysrd/calculation-service/internal/pkg/domain/user"
+)
+
+type UserController struct {
+	service user.Service
+}
+
+type UserControllerOptions struct {
+	Service user.Service
+}
+
+func NewUserController(options UserControllerOptions) *UserController {
+	return &UserController{
+		service: options.Service,
+	}
+}
+
+func (c *UserController) RegisterRoutes(gin *gin.RouterGroup) {
+	gin.GET("me", c.FindMe)
+	gin.DELETE("me", c.DeleteMe)
+}
+
+func (c *UserController) FindMe(ctx *gin.Context) {
+	user, err := c.service.FindByUserName("admin@test.com")
+
+	if err != nil {
+		ctx.JSON(400, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(200, user)
+}
+
+func (c *UserController) DeleteMe(ctx *gin.Context) {
+	err := c.service.Delete("admin@test.com")
+
+	if err != nil {
+		ctx.JSON(400, gin.H{
+			"code":    400,
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
+}
