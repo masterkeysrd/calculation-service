940de68dda7c36e0e050e10829adb685a9a0acdf
David Morales <morales.david1997@gmail.com>
2023-05-20 02:13:36 -0400

    feat(infra/config): :sparkles: add config implementation

---
+++ internal/pkg/infra/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"github.com/gookit/config/v2"
+	"github.com/gookit/config/v2/yaml"
+	"github.com/masterkeysrd/calculation-service/internal/pkg/infra/jwt"
+)
+
+type Config struct {
+	JWT *jwt.Config `mapstructure:"jwt"`
+}
+
+func LoadConfig() {
+	config.WithOptions(config.ParseEnv)
+	config.AddDriver(yaml.Driver)
+
+	// TODO: Change the path to a ENV variable
+	err := config.LoadFiles("../../config/server.yml")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetConfig() *Config {
+	cfg := &Config{}
+	config.Decode(cfg)
+	return cfg
+}
+
+func GetJWTConfig(config *Config) *jwt.Config {
+	return config.JWT
+}
